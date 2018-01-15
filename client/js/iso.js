e4f38929fe4be0d26ac8424af07305e60fa44fbc
Bruno Antão <brunomantao@gmail.com>
2018-01-15 19:58:05 +0000

    cube side detect added

--- client/js/iso.js
+++ client/js/iso.js
@@ -78,12 +78,6 @@ class IsoFace extends PIXI.Graphics {
 
         this.interactive = true;
 
-        this.on('pointerdown', function (face) {
-
-            console.log('s');
-
-        });
-
         this.on('pointerover', function (face) {
 
             this.color = this.overColor;
@@ -143,6 +137,126 @@ class IsoPoint {
 
         this.tx = 300 / 4 + this.x * 32 + this.y * 32;
         this.ty = 800 / 4 + this.x * 16 * this.prespective - this.y * 16 * this.prespective - this.z * 32;
+
+    }
+
+}
+
+class Grid extends IsoPoly {
+
+    constructor(app, x, y, z) {
+
+        let parent;
+
+        let faces = [
+
+            new GridFace(parent, { x: 0, y: 0, z: -1 }, [
+
+                new IsoPoint(0, 0, 0),
+                new IsoPoint(1, 0, 0),
+                new IsoPoint(1, 1, 0),
+                new IsoPoint(0, 1, 0)
+
+            ]),
+
+            new GridFace(parent, { x: -1, y: 0, z: 0 }, [
+
+                new IsoPoint(0, 0, 0),
+                new IsoPoint(0, 1, 0),
+                new IsoPoint(0, 1, 1),
+                new IsoPoint(0, 0, 1)
+
+            ]),
+
+            new GridFace(parent, { x: 0, y: 1, z: 0 }, [
+
+                new IsoPoint(0, 1, 0),
+                new IsoPoint(1, 1, 0),
+                new IsoPoint(1, 1, 1),
+                new IsoPoint(0, 1, 1)
+
+            ]),
+
+            new GridFace(parent, { x: 0, y: -1, z: 0 }, [
+
+                new IsoPoint(0, 0, 0),
+                new IsoPoint(1, 0, 0),
+                new IsoPoint(1, 0, 1),
+                new IsoPoint(0, 0, 1)
+
+            ]),
+
+            new GridFace(parent, { x: 1, y: 0, z: 0 }, [
+
+                new IsoPoint(1, 0, 0),
+                new IsoPoint(1, 1, 0),
+                new IsoPoint(1, 1, 1),
+                new IsoPoint(1, 0, 1)
+
+            ]),
+
+            new GridFace(parent, { x: 0, y: 0, z: 1 }, [
+
+                new IsoPoint(0, 0, 1),
+                new IsoPoint(1, 0, 1),
+                new IsoPoint(1, 1, 1),
+                new IsoPoint(0, 1, 1)
+
+            ])
+
+        ]
+
+        super(app, x, y, z, faces);
+
+        parent = this;
+
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+    }
+
+}
+
+class GridFace extends IsoFace {
+
+    constructor(parent, vector, points) {
+
+        super(points);
+
+        this.parent = parent;
+        this.vector = vector;
+
+        this.on('pointerdown', function (face) {
+
+            global.tiles.addChild(new Grid(global.app, this.parent.x + this.vector.x, this.parent.y + this.vector.y, this.parent.z + this.vector.z));
+
+        });
+
+    }
+
+    draw() {
+
+        this.clear();
+
+        this.beginFill(this.color, 0.1);
+        this.lineStyle(1, 0xffffff, 1);
+
+        this.moveTo(this.points[0].tx, this.points[0].ty);
+
+        this.points.forEach(function (point, i) {
+
+            if (i > 0) {
+
+                this.lineTo(point.tx, point.ty);
+
+            }
+
+        }, this);
+
+        this.lineTo(this.points[0].tx, this.points[0].ty);
+
+        this.endFill();
 
     }
 
